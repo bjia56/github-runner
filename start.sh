2f68c9ef41ea3e1fae3fc62869fd320f3377acdd
Brett Jia <dev.bjia56@gmail.com>
2025-05-30 14:48:47 -0400

    unset exported environment variables

--- start.sh
+++ start.sh
@@ -7,6 +7,10 @@ echo "REPO ${REPOSITORY}"
 
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
+unset REPO
+unset TOKEN
+unset ACCESS_TOKEN
+
 cd /home/docker/actions-runner
 
 ./config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN} --ephemeral
